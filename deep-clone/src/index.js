cb754a65e2254646914e1c0f23cba25839c2a2f7
sean <770899447@qq.com>
2022-05-13 03:59:46 +0800

    depp-clone

---
+++ deep-clone/src/index.js
@@ -0,0 +1,48 @@
+class DeepClone {
+  constructor() {
+    this.cache = [];
+  }
+
+  clone(source) {
+    if (source instanceof Object) {
+      const cacheDist = this.findCache(source);
+      if (cacheDist) {
+        return cacheDist;
+      } else {
+        let dist;
+        if (source instanceof Array) {
+          dist = new Array();
+        } else if (source instanceof RegExp) {
+          dist = new RegExp(source.source, source.flags);
+        } else if (source instanceof Date) {
+          dist = new Date(source);
+        } else if (source instanceof Function) {
+          dist = function () {
+            return source.apply(this, arguments);
+          };
+        } else {
+          dist = new Object();
+        }
+        this.cache.push([source, dist]);
+        for (let k in source) {
+          if (source.hasOwnProperty(k)) {
+            dist[k] = this.clone(source[k]);
+          }
+        }
+        return dist;
+      }
+    }
+
+    return source;
+  }
+  findCache(source) {
+    for (let i = 0; i < this.cache.length; i++) {
+      if (this.cache[i][0] === source) {
+        return this.cache[i][1];
+      }
+    }
+    return undefined;
+  }
+}
+
+module.exports = DeepClone;
