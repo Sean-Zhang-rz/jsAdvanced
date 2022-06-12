821a153f46e8d0a0af0a8ca11d755b5f1e254653
sean <770899447@qq.com>
2022-06-12 21:22:42 +0800

    转换es5代码

---
+++ Webpack/to_es5.ts
@@ -0,0 +1,12 @@
+import { parse } from '@babel/parser';
+import * as babel from '@babel/core';
+import * as fs from 'fs';
+
+const path = require('path');
+let file = path.join(__dirname, './fsFile/index.ts');
+const code = fs.readFileSync(file).toString();
+const ast = parse(code, { sourceType: 'module' });
+const result = babel.transformFromAstSync(ast, code, {
+  presets: ['@babel/preset-env'],
+});
+fs.writeFileSync('./WebPack/index.es5.js', result.code);
