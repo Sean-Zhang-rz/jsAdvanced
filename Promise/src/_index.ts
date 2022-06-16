2d9e409374dcc9cfff86b6a5244fefbc2e2aefd2
sean <770899447@qq.com>
2022-06-16 08:31:13 +0800

    then链

--- Promise/src/_index.ts
+++ Promise/src/_index.ts
@@ -1,12 +1,12 @@
+interface handlerProps {
+  onResolve: (res) => void;
+  onReject: (rej) => void;
+  handleNext: _Promise;
+}
+type State = 'pending' | 'fullfilled' | 'rejected';
 class _Promise {
-  state: 'pending' | 'fullfilled' | 'rejected' = 'pending';
-  handler: {
-    onResolve: (res) => void;
-    onReject: (rej) => void;
-  } = {
-    onResolve: (res) => {},
-    onReject: (rej) => {},
-  };
+  state: State = 'pending';
+  callbacks: handlerProps[] = [];
 
   constructor(fn) {
     if (typeof fn !== 'function') throw new Error('Promise必须传入一个函数');
@@ -21,19 +21,38 @@ class _Promise {
     if (this.state !== 'pending') return;
     this.state = 'fullfilled';
     queueMicrotask(() => {
-      if (this.handler.onResolve) this.handler.onResolve(result);
+      this.callbacks.forEach((cb) => {
+        if (cb.onResolve) {
+          const x = cb.onResolve(result);
+          // 执行next
+          cb.handleNext.resolve(x);
+        }
+      });
     });
   }
   reject(reason) {
     if (this.state !== 'pending') return;
     this.state = 'rejected';
     queueMicrotask(() => {
-      if (this.handler.onReject) this.handler.onReject(reason);
+      this.callbacks.forEach((cb) => {
+        if (cb.onReject) {
+          const x = cb.onReject(reason);
+          // 执行next
+          cb.handleNext.reject(x);
+        }
+      });
     });
   }
   then(onResolve?, onReject?) {
-    if (typeof onResolve === 'function') this.handler.onResolve = onResolve;
-    if (typeof onReject === 'function') this.handler.onReject = onReject;
+    const handler: handlerProps = {
+      onResolve: (res) => {},
+      onReject: (rej) => {},
+      handleNext: new _Promise(() => {}),
+    };
+    if (typeof onResolve === 'function') handler.onResolve = onResolve;
+    if (typeof onReject === 'function') handler.onReject = onReject;
+    this.callbacks.push(handler);
+    return handler.handleNext;
   }
   catch(onReject?) {
     this.then(null, onReject);
@@ -41,15 +60,19 @@ class _Promise {
 }
 
 const p1 = new _Promise((res, rej) => {
-  // res(1);
-  throw Error('12121');
+  res(1);
+  // throw Error('12121');
 });
 
-p1.then(
-  (res) => {
+p1.then((res) => {
+  console.log(res);
+  return 2;
+})
+  .then((res) => {
+    console.log(2);
     console.log(res);
-  },
-  (rej) => {
-    console.log(rej);
-  }
-);
+  })
+  .then((res) => {
+    console.log(3);
+    console.log(res);
+  });
