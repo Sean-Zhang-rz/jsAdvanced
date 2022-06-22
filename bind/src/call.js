c28811568f72f177acd962a590d6f4653ec0f68c
sean <770899447@qq.com>
2022-06-22 17:21:13 +0800

    手写call

---
+++ bind/src/call.js
@@ -0,0 +1,16 @@
+// es5版
+Function.prototype._call = function (ctx) {
+  let _this = ctx
+  if (!ctx) {
+    _this = typeof window === 'undefined' ? global : window
+  }
+  _this.fn = this
+  const args = []
+  for(let i = 1, len = arguments.length; i < len; i ++) {
+    args.push(`arguments[${i}]`)
+  }
+  const res = `_this.fn(${args})`
+  const result = eval(res)
+  delete _this.fn
+  return result
+}
