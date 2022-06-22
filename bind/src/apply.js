23599eb2b577eec364993d5d77811867b04c644a
sean <770899447@qq.com>
2022-06-22 17:36:51 +0800

    apply实现

---
+++ bind/src/apply.js
@@ -0,0 +1,18 @@
+// es5版
+Function.prototype._apply = function (ctx, params) {
+  let _this = ctx
+  if (!ctx) {
+    _this = typeof window === 'undefined' ? global : window
+  }
+  _this.fn = this
+  const args = []
+  if (params instanceof Array) {
+    for (let i = 0, len = params.length; i < len; i++) {
+      args.push(`params[${i}]`)
+    }
+  }
+  const res = `_this.fn(${args})`
+  const result = eval(res)
+  delete _this.fn
+  return result
+}
