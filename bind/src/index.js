dace658644db99d4e96de800d5d7c6f212e9c2f9
sean <770899447@qq.com>
2022-06-23 10:03:58 +0800

    bind

--- bind/src/index.js
+++ bind/src/index.js
@@ -1,9 +1,10 @@
 var slice = Array.prototype.slice;
 // 使用旧api，不能new
 function bind2(_this) {
+  console.log('this', this);
   var fn = this;
   var args = slice.call(arguments, 1);
-  const resultFn = function (...args2) {
+  const resultFn = function () {
     var args2 = slice.call(arguments, 0);
     return fn.apply(resultFn.prototype.isPrototypeOf(this) ? this : _this, args.concat(args2));
   };
@@ -14,6 +15,8 @@ function bind2(_this) {
 function _bind2(_this, ...args) {
   var fn = this;
   const resultFn = function (...args2) {
+    console.log('this', this);
+    // 判断是否为new调用的，这句return会覆盖new的默认return this, 参数就会绑定到window/global上
     return fn.call(resultFn.prototype.isPrototypeOf(this) ? this : _this, ...args, ...args2);
   };
   resultFn.prototype = fn.prototype;
@@ -22,6 +25,18 @@ function _bind2(_this, ...args) {
 
 module.exports = bind2;
 
-if (!Function.prototype.bind) {
-  Function.prototype.bind = bind2;
+Function.prototype.bind2 = _bind2;
+var name = 'Jack';
+
+function person(age, job, gender) {
+  this.age = age
+  this.job = job
+  this.gender = gender
+  console.log(this.name, age, job, gender);
 }
+var Yve = {
+  name: 'Yvette'
+};
+let result = person.bind2(Yve, 22, 'enginner')
+const obj = new result()
+console.log(obj);
