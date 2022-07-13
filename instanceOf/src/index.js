380561c0da819aca8f9cba60715058a9590a1a73
sean <770899447@qq.com>
2022-07-13 19:13:01 +0800

    实现instanceof

---
+++ instanceOf/src/index.js
@@ -0,0 +1,10 @@
+function _instanceof(left, right){
+  let prototype = right.prototype;
+  let l = left.__proto__;
+  while(true) {
+    if (l == null) return false;
+    if (prototype === l) return true;
+    l = l.__proto__;
+  }
+}
+console.log(_instanceof(Function, Object));
--- new/src/index.js
+++ new/src/index.js
@@ -5,3 +5,5 @@ function _new() {
   let result = Con.apply(obj, arguments);
   return result instanceof Object ? result : obj;
 }
+
+const p = _new(Parent)
