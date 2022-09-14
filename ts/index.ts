41f69bc919efbc997b1fa1ecc4ecd1c1286f846d
sean <770899447@qq.com>
2022-09-14 17:44:36 +0800

    ts学习1

---
+++ ts/index.ts
@@ -0,0 +1,20 @@
+const fn = (a: any): boolean => {
+  // if (typeof a === 'string') {
+  //   a
+  // } else if (a instanceof Date) {
+  //   a
+  // }
+  a.toString()
+  a.toFixed(2)
+  return true
+}
+const fn1 = (a: unknown): boolean => {
+  // if (typeof a === 'string') {
+  //   a
+  // } else if (a instanceof Date) {
+  //   a
+  // }
+  a.toString()
+
+  return true
+}
