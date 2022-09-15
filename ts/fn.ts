5108df015ac09ba03794e5a3dd7739d65f953738
sean <770899447@qq.com>
2022-09-15 16:43:12 +0800

    ts类型兼容

---
+++ ts/fn.ts
@@ -0,0 +1,17 @@
+interface MyEvent { target: string }
+interface MyMouseEvent extends MyEvent { x: number; y: number }
+function listener(eType: string, fn: (n: MyEvent) => void) { 
+  if (eType==='click') {
+    fn({ target:'s'})
+  }
+}
+// 报错
+listener('click', (e: MyMouseEvent) => console.log(e.x + ' ' + e.y))
+// ok
+listener('click', (e: MyEvent) => {
+  console.log((e as MyMouseEvent).x + ' ' + (e as MyMouseEvent).y)
+})
+// 也ok，但不常用
+// listener('click', ((e: MyMouseEvent) => console.log(e.x + ' ' + e.y)) as (e: myEvent) => void)
+// 过于离谱
+// listener('click', (e: numebr) => console.log(e))
