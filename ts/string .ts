5a3e1b9814a7bcef1dee5bc09947e7df0af4c7c3
seanMr <770899447@qq.com>
2022-09-21 22:28:13 +0800

    字符串体操

---
+++ ts/string .ts
@@ -0,0 +1,23 @@
+// Uppercase Lowercase Uncapitalize Capitalize
+type A = 'sean';
+type B = Capitalize<A>;
+
+type C = 'a' | 'b' | 'c';
+type D = Capitalize<C>;
+
+// 组合字符串
+type E = 'a';
+type F = 'b';
+type G = `${E} ${F}`; // a b
+
+// 获取第一个字符
+type First<T extends string> = T extends `${infer F}${string}` ? F : never;
+type Result = First<'ji ni tai mei'>; // j
+// 获取最后一个字符
+// 先将字符串转换成元祖，再获取元祖最后一项
+type Last<T extends unknown[]> = T extends [...infer First, infer Last] ? Last : never;
+type StringToTuple<S extends string> = S extends `${infer F}${infer R}`
+  ? [F, ...StringToTuple<R>]
+  : [];
+type LastOfString<S extends string> = Last<StringToTuple<S>>;
+export {};
--- ts/tuple.ts
+++ ts/tuple.ts
@@ -7,3 +7,4 @@ export {};
 type D = [1, 2, 3, 4];
 type Last<T extends unknown[]> = T extends [...infer First, infer Last] ? Last : never;
 type L = Last<D>;
+export {};
