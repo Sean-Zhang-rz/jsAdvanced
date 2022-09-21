238ac14ea5e5a10ebb1bf988d7102f648d77b027
seanMr <770899447@qq.com>
2022-09-21 22:07:48 +0800

    元祖操作

---
+++ ts/tuple.ts
@@ -0,0 +1,9 @@
+// 元祖拓展
+type A = [number, string];
+type B = [...A, string];
+type C = [...A, ...B];
+export {};
+// 元祖缩窄
+type D = [1, 2, 3, 4];
+type Last<T extends unknown[]> = T extends [...infer First, infer Last] ? Last : never;
+type L = Last<D>;
