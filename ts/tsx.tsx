f4d53e0998908c1a2dc8df79fe54e6b84282882f
seanMr <770899447@qq.com>
2022-09-22 08:39:21 +0800

    tsc初体验

---
+++ ts/tsx.tsx
@@ -0,0 +1,16 @@
+const header = <h1 name={123}>hi</h1>;
+header.tag;
+declare global {
+  namespace JSX {
+    interface Element {
+      tag: string[];
+    }
+    interface IntrinsicElements {
+      h1: {
+        name: number;
+      };
+    }
+  }
+}
+
+export {};
