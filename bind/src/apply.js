5d05c5cb148530d06db1075bed09ce45833da0fa
seanZhang <770899447@qq.com>
2022-12-05 21:52:10 +0800

    格式

--- bind/src/apply.js
+++ bind/src/apply.js
@@ -1,18 +1,19 @@
 // es5版
-Function.prototype._apply = function (ctx, params) {
-  let _this = ctx
+const _apply = function (ctx, params) {
+  let _this = ctx;
   if (!ctx) {
-    _this = typeof window === 'undefined' ? global : window
+    _this = typeof window === 'undefined' ? global : window;
   }
-  _this.fn = this
-  const args = []
+  _this.fn = this;
+  const args = [];
   if (params instanceof Array) {
     for (let i = 0, len = params.length; i < len; i++) {
-      args.push(`params[${i}]`)
+      args.push(`params[${i}]`);
     }
   }
-  const res = `_this.fn(${args})`
-  const result = eval(res)
-  delete _this.fn
-  return result
-}
+  const res = `_this.fn(${args})`;
+  const result = eval(res);
+  delete _this.fn;
+  return result;
+};
+export default _apply;
--- bind/src/call.js
+++ bind/src/call.js
@@ -1,16 +1,18 @@
 // es5版
-Function.prototype._call = function (ctx) {
-  let _this = ctx
+const _call = function (ctx) {
+  let _this = ctx;
   if (!ctx) {
-    _this = typeof window === 'undefined' ? global : window
+    _this = typeof window === 'undefined' ? global : window;
   }
-  _this.fn = this
-  const args = []
-  for(let i = 1, len = arguments.length; i < len; i ++) {
-    args.push(`arguments[${i}]`)
+  _this.fn = this;
+  const args = [];
+  for (let i = 1, len = arguments.length; i < len; i++) {
+    args.push(`arguments[${i}]`);
   }
-  const res = `_this.fn(${args})`
-  const result = eval(res)
-  delete _this.fn
-  return result
-}
+  const res = `_this.fn(${args})`;
+  const result = eval(res);
+  delete _this.fn;
+  return result;
+};
+
+export default _call;
